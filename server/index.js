3c2e87fa19c478c05d64df3b252b07dd42e922ec
Mohamed Rayen Toujani <mo.rayen.toujani@gmail.com>
2024-07-01 04:02:06 +0100

    front setup

--- server/index.js
+++ server/index.js
@@ -8,13 +8,16 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes.auth.js"
+import authRoutes from "./routes/auth.js"
 import { error } from "console";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
 import { createPost}from "./controllers/posts.js";
-import userRoutes from "./routes.users.js";
-import postRoutes from "./routes.post.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import User from "./models/User.js";
+import Post from "./models/post.js";
+import {users,posts} from "./data/index.js";
 
 
 
@@ -57,6 +60,8 @@ mongoose.connect(process.env.MONGO_URL,{
     useUnifiedTopology:true,
 
 }).then(()=>{
-    app.listen(PORT , ()=>console.log(`listening on port ${PORT}`))
+    app.listen(PORT , ()=>console.log(`listening on port ${PORT}`));
+  
+
 }).catch((error)=>console.log(error));
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -7,7 +7,7 @@ import {
 }   from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
-const router = express.Router;
+const router = express.Router();
 
 router.get("/:id",verifyToken,getUser);
 router.get("/:id/friends",verifyToken,getUserFriends);
